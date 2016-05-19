e2ff816bfb0888e625f0c1feaf7dbd8c35e5ff0c
DaCyclops <DaCyclops@users.noreply.github.com>
2016-05-19 15:31:36 +1000

    Update gdiw.lua

--- gdiw.lua
+++ gdiw.lua
@@ -1,15 +1,5 @@
 -- adds functions for GDIW compatibility in blueprint flipping
 
-local function checkGDIW()
-  -- checks if GDIW installed, checked on script load
-  if data.raw["recipe"]["advanced-oil-processing-GDIW-3"] then
-    return true
- else
-    return false
-  end
-end
-
-
 
 local function flipGDIWrecipe(en)
   -- flips recipes
