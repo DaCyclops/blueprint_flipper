719561d18fef41e425ff10c4376fe0f9f44b4ad8
DaCyclops <DaCyclops@users.noreply.github.com>
2016-05-19 15:25:15 +1000

    whoops, equal'd
    Turns out, I forgot the second = again

--- gdiw.lua
+++ gdiw.lua
@@ -13,45 +13,45 @@ end
 
 local function flipGDIWrecipe(en)
   -- flips recipes
-  if en.recipe = "basic-oil-processing" 
+  if en.recipe=="basic-oil-processing" 
     return "basic-oil-processing-GDIW-3"
-  elseif en.recipe = "basic-oil-processing-GDIW-3" 
+  elseif en.recipe=="basic-oil-processing-GDIW-3" 
     return "basic-oil-processing"
     
-  elseif en.recipe = "advanced-oil-processing" 
+  elseif en.recipe=="advanced-oil-processing" 
     return "advanced-oil-processing-GDIW-2"
-  elseif en.recipe = "advanced-oil-processing-GDIW-2" 
+  elseif en.recipe=="advanced-oil-processing-GDIW-2" 
     return "advanced-oil-processing"
     
-  elseif en.recipe = "advanced-oil-processing-GDIW" 
+  elseif en.recipe=="advanced-oil-processing-GDIW" 
     return "advanced-oil-processing-GDIW-3"
-  elseif en.recipe = "advanced-oil-processing-GDIW-3" 
+  elseif en.recipe=="advanced-oil-processing-GDIW-3" 
     return "advanced-oil-processing-GDIW"
     
-  elseif en.recipe = "heavy-oil-cracking-GDIW"
+  elseif en.recipe=="heavy-oil-cracking-GDIW"
     return "heavy-oil-cracking"
-  elseif en.recipe = "heavy-oil-cracking"
+  elseif en.recipe=="heavy-oil-cracking"
     return "heavy-oil-cracking-GDIW"
     
-  elseif en.recipe = "light-oil-cracking"
+  elseif en.recipe=="light-oil-cracking"
     return "light-oil-cracking-GDIW"
-  elseif en.recipe = "light-oil-cracking-GDIW"
+  elseif en.recipe=="light-oil-cracking-GDIW"
     return "light-oil-cracking"
 
-  elseif en.recipe = "sulfur"
+  elseif en.recipe=="sulfur"
     return "sulfur-GDIW"
-  elseif en.recipe = "sulfur-GDIW"
+  elseif en.recipe=="sulfur-GDIW"
     return "sulfur"
 
-  elseif en.recipe = "flame-thrower-ammo-GDIW"
+  elseif en.recipe=="flame-thrower-ammo-GDIW"
     return "flame-thrower-ammo"
-  elseif en.recipe = "flame-thrower-ammo"
+  elseif en.recipe=="flame-thrower-ammo"
     return "flame-thrower-ammo-GDIW"
 
   -- Should be fine, cant match something not installed
-  if en.recipe = "bob-oil-processing" 
+  if en.recipe=="bob-oil-processing" 
     return "bob-oil-processing-GDIW"
-  elseif en.recipe = "bob-oil-processing-GDIW" 
+  elseif en.recipe=="bob-oil-processing-GDIW" 
     return "bob-oil-processing"
 
   else
